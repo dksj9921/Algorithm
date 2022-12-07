fe979b0c6635e37d906897ff5b4f3bb2b706f014
SungHwan <sung980213@naver.com>
2022-12-07 15:33:27 +0900

    알바가기 싫다

--- src/practice20.java
+++ src/practice20.java
@@ -1,11 +1,22 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class practice20 {
 
-    public static void main(String Args[]) throws NumberFormatException, IOException{
+    public static void main(String Args[]) throws NumberFormatException, IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bf.readLine());
+
+        int arr[] = new int[N];
+
+        for (int i : arr) {
+            arr[i] = Integer.parseInt(bf.readLine());
+        }
 
 
     }
-
-
+    static void MargeSort()
 }
