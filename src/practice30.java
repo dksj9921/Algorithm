8a821aeaa28fc11176b464f70e162f4a3436246f
dksj9921 <83447917+dksj9921@users.noreply.github.com>
2022-12-14 03:30:27 +0900

    temp

--- src/practice30.java
+++ src/practice30.java
@@ -5,7 +5,7 @@ import java.util.StringTokenizer;
 
 public class practice30 {
 
-    public static void main(String Args[])throws NumberFormatException, IOException {
+    public static void main(String Args[]) throws NumberFormatException, IOException {
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
@@ -17,35 +17,36 @@ public class practice30 {
         int sum = 0;
         int max = 0;
         st = new StringTokenizer(bf.readLine());
-        for(int i =0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
             sum += arr[i];
-            if(arr[i]>max) max=arr[i];
+            if (arr[i] > max) max = arr[i];
         }
 
         int start = max;
         int end = sum;
         int count = 0;
+        int middle = 0;
 
 
-        while(start<=end){
-            int middle = (start+end)/2;
+        while (start <= end) {
+            middle = (start + end) / 2;
             int arrSum = 0;
-            count =0;
-            for(int i =0; i<N;i++){
-                if(arrSum+arr[i]>=middle){
+            count = 0;
+            for (int i = 0; i < N; i++) {
+                if (arrSum + arr[i] > middle) {
                     count++;
                     arrSum = 0;
                 }
-                arrSum=arrSum+arr[i];
+                arrSum = arrSum + arr[i];
             }
-            if(arrSum!=0) count++;
+            if (arrSum != 0) count++;
 
-            if(count>M) start = middle+1;
+            if (count > M) start = middle + 1;
             else end = middle - 1;
         }
 
-        System.out.println(start-1);
+        System.out.println(start);
 
     }
 
---
+++ src/practice31.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class practice31 {
+
+    public static void main(String Args[])throws NumberFormatException, IOException{
+        
+    }
+}
