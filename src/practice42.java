400dc49015ab06b57a22348e9ddfc081fa41cab9
SungHwan <sung980213@naver.com>
2022-12-20 16:03:16 +0900

    코테

---
+++ src/practice42.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class practice42 {
+
+    public static void main(String Args[]) throws NumberFormatException, IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+
+
+        for (int i = 0; i < N; i++) {
+            int s = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+
+            char arr[] = bf.readLine().toCharArray();
+
+
+        }
+    }
+
+    public static class Node{
+        int value;
+        int
+    }
+}
