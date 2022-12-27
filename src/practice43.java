7c62d70373bf387f372add826ea3d7cc64e42bf6
SungHwan <sung980213@naver.com>
2022-12-27 11:08:12 +0900

    practice43완료

--- src/practice43.java
+++ src/practice43.java
@@ -1,32 +1,44 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class practice43 {
 
-    public static void main(String Args[])throws NumberFormatException, IOException{
+    public static void main(String Args[]) throws NumberFormatException, IOException {
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
 
-        int big = 0;
-        int small = 0;
+        long big = 0;
+        long small = 0;
 
-        if(A>B) {
+        if (A > B) {
             big = A;
             small = B;
-        }
-        else if(B>A){
+        } else {
             big = B;
             small = A;
         }
 
-        int minus = big - small;
+        long mod = 1;
+
+        while(mod!=0){
+            mod = big % small;
+            big = small;
+            if(mod!=0) small = mod;
+        }
+
+        for (long i = 0; i < small; i++) {
+            String s = "1";
+            bw.write(s);
+        }
+
+        bw.flush();
+        bw.close();
+        bf.close();//스트림을 닫음
 
     }
-
 }
---
+++ src/practice44.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class practice44 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        
+
+    }
+}
