4c8024c048c52bc4a45ad46e0b7c1fdd41f0b1ca
SungHwan <sung980213@naver.com>
2023-01-12 07:05:51 +0900

    practice58 진행중

--- src/Do_It_Algorithm_Coding_Test/practice55.java
+++ src/Do_It_Algorithm_Coding_Test/practice55.java
@@ -16,12 +16,12 @@ public class practice55 {
 
         int arrival_TimeArr[] = new int[N + 1];
         List<Node> route[] = new List[N + 1];
-        int arrival_CountArr[] = new int[N + 1];
-
+        List<Node> route_Reverse[] = new List[N + 1];
         Queue<Integer> myQueue = new LinkedList<>();
 
         for (int i = 1; i < N + 1; i++) {
             route[i] = new ArrayList<>();
+            route_Reverse[i] = new ArrayList<>();
         }
 
         int count[] = new int[N + 1];
@@ -34,6 +34,7 @@ public class practice55 {
             int temp_Arrival_Time = Integer.parseInt(st.nextToken());
 
             route[start].add(new Node(end, temp_Arrival_Time));
+            route_Reverse[end].add(new Node(start, temp_Arrival_Time));
             count[end]++;
         }
         st = new StringTokenizer(bf.readLine());
@@ -50,11 +51,6 @@ public class practice55 {
                 int temp = arrival_TimeArr[next.index];
                 arrival_TimeArr[next.index] = Math.max(arrival_TimeArr[next.index], arrival_TimeArr[now] + next.arrival_Time);
 
-                if (arrival_TimeArr[next.index] == arrival_TimeArr[now] + next.arrival_Time) {
-                    if(arrival_TimeArr[next.index] == temp){
-                        arrival_CountArr[next.index] = arrival_CountArr[now] + arrival_CountArr[next.index] + 1;
-                    }else arrival_CountArr[next.index] = arrival_CountArr[now] + 1;
-                }
                 count[next.index]--;
                 if (count[next.index] == 0) {
                     myQueue.add(next.index);
@@ -62,8 +58,30 @@ public class practice55 {
             }
         }
 
+        myQueue = new LinkedList<>();
+
+        myQueue.add(end_node_index);
+        int reverse_Count = 0;
+
+        boolean visited[] = new boolean[N + 1];
+
+        while (!myQueue.isEmpty()) {
+            int now = myQueue.poll();
+
+            for (Node next : route_Reverse[now]) {
+                if (((arrival_TimeArr[next.index] + next.arrival_Time) == arrival_TimeArr[now])) {
+                    reverse_Count++;
+                    if (!visited[next.index]) {
+                        visited[next.index] = true;
+                        myQueue.add(next.index);
+                    }
+                }
+            }
+        }
+
+
         System.out.println(arrival_TimeArr[end_node_index]);
-        System.out.println(arrival_CountArr[end_node_index]);
+        System.out.println(reverse_Count);
     }
 
     public static class Node {
---
+++ src/Do_It_Algorithm_Coding_Test/practice56.java
@@ -0,0 +1,81 @@
+package Do_It_Algorithm_Coding_Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class practice56 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        int MAX_SIZE = 99999999;
+
+        List<Node> arr[] = new List[V + 1];
+        int weight_arr[] = new int[V + 1];
+        boolean visited[] = new boolean[V + 1];
+
+        for (int i = 1; i <= V; i++) {
+            arr[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < V + 1; i++) {
+            weight_arr[i] = Integer.MAX_VALUE;
+        }
+
+        int start_Node = Integer.parseInt(bf.readLine());
+        weight_arr[start_Node] = 0;
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(bf.readLine());
+
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            int weight = Integer.parseInt(st.nextToken());
+
+            arr[start].add(new Node(end, weight));
+        }
+
+        PriorityQueue<Node> myQueue = new PriorityQueue<>();
+
+        myQueue.add(new Node(start_Node, 0));
+
+        while (!myQueue.isEmpty()) {
+            Node now = myQueue.poll();
+            if(visited[now.index]) continue;
+            visited[now.index] = true;
+            for (Node next : arr[now.index]) {
+                if (weight_arr[next.index] > (weight_arr[now.index] + next.value)) {
+                    weight_arr[next.index] = (weight_arr[now.index] + next.value);
+                    myQueue.add(new Node(next.index, weight_arr[next.index]));
+                }
+            }
+        }
+
+        for (int i = 1; i < V + 1; i++) {
+            if (!visited[i]) {
+                System.out.println("INF");
+            } else System.out.println(weight_arr[i]);
+        }
+    }
+
+    public static class Node implements Comparable<Node> {
+        int index;
+        int value;
+
+        public Node(int index, int value) {
+            this.index = index;
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(Node input) {
+            if (this.value > input.value) return 1;
+            else return -1;
+        }
+    }
+}
