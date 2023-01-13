359b4707ffbc23c9444e71eb7ba2da9c267ab342
SungHwan <sung980213@naver.com>
2023-01-13 23:02:04 +0900

    임시커밋

--- src/Do_It_Algorithm_Coding_Test/practice58.java
+++ src/Do_It_Algorithm_Coding_Test/practice58.java
@@ -3,6 +3,7 @@ package Do_It_Algorithm_Coding_Test;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class practice58 {
@@ -18,13 +19,11 @@ public class practice58 {
 
         List<Node> arr[] = new List[n + 1];
         Queue<Node> myQueue = new LinkedList<>();
-        HashSet<Integer> answer[] = new HashSet[n + 1];
         int weight_Arr[] = new int[n + 1];
 
 
         for (int i = 0; i < n + 1; i++) {
             arr[i] = new ArrayList<>();
-            answer[i] = new HashSet<>();
         }
 
         for (int i = 0; i < m; i++) {
@@ -37,38 +36,7 @@ public class practice58 {
             arr[start].add(new Node(end, weight));
         }
 
-        myQueue.add(new Node(1, 0));
 
-        while (!myQueue.isEmpty()) {
-            Node now = myQueue.poll();
-            int now_value = now.value;
-
-            for (Node next : arr[now.index]) {
-                if (weight_Arr[next.index] != (now_value + next.value)) {
-                    answer[next.index].add(now_value + next.value);
-                    if (answer[next.index].size() <= k + 2) {
-                        myQueue.add(new Node(next.index, now_value + next.value));
-                    }
-                }
-            }
-        }
-
-        for (int i = 1; i < n + 1; i++) {
-            if(answer[i].size()<k){
-                sb.append(-1 + "\n");
-            } else {
-                answer[i].stream().sorted();z
-                Iterator<Integer> iter = answer[i].iterator();
-                for (int j = 0; j < k; j++) {
-                    int temp = iter.next();
-                    if (j == k - 1) {
-                        sb.append(temp + "\n");
-                    }
-                }
-            }
-
-
-        }
 
         System.out.println(sb);
     }
