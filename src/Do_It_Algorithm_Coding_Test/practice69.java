f49e5a58d902f557604a148dc27a4eac016adf9c
dksj9921 <83447917+dksj9921@users.noreply.github.com>
2023-01-22 18:03:49 +0900

    practice69 진행중

---
+++ src/Do_It_Algorithm_Coding_Test/practice69.java
@@ -0,0 +1,12 @@
+package Do_It_Algorithm_Coding_Test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class practice69 {
+    public static void main(String[] args) {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
